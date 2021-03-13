f6c6f344c397ef829a76ec58b249c59490ab03b9
March <lena_marenich@mail.ru>
2021-03-14 00:40:13 +0300

    Delete function and others

--- backend/app.js
+++ backend/app.js
@@ -46,7 +46,14 @@ app.get('/api/posts', (req, res) => {
     });
   });
 
-
 });
+
+app.delete('/api/posts/:id', (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then( () => {
+    // console.log(result);
+    res.status(200).json({ message: "Post deleted!" });
+});
+
+})
 
 module.exports = app;
